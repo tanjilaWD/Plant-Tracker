11eeb72e6425a33a06f5eb818729c49dc2b2a283
Tanjila Khatun <tanjilabithi12@gmail.com>
2026-02-21 17:53:40 +0600

    feat: code added

--- script.js
+++ script.js
@@ -5,8 +5,12 @@ let total = document.getElementById('total');
 let thrivingCount = document.getElementById('thrivingCount');
 let strugglingCount = document.getElementById('strugglingCount');
 
-const allCardSection = document.getElementById('allCards');
+const allFilterBtn = document.getElementById('all-filter-btn');
+const thrivingFilterBtn = document.getElementById('thriving-filter-btn');
+const strugglingFilterBtn = document.getElementById('struggling-filter-btn');
 
+
+const allCardSection = document.getElementById('allCards');
 const mainContainer = document.querySelector('main');
 
 function calculateCount(){
@@ -18,5 +22,19 @@ function calculateCount(){
 calculateCount();
 
 function toggleStyle(id){
-    console.log('click');
+   allFilterBtn.classList.remove('bg-black', 'text-white');
+   thrivingFilterBtn.classList.remove('bg-black', 'text-white');
+   strugglingFilterBtn.classList.remove('bg-black', 'text-white');
+
+   allFilterBtn.classList.add('bg-gray-300', 'text-black');
+   thrivingFilterBtn.classList.add('bg-gray-300', 'text-black');
+   strugglingFilterBtn.classList.add('bg-gray-300', 'text-black');
+
+   const selected = document.getElementById(id);
+   selected.classList.remove('bg-gray-300', 'text-black');
+   selected.classList.add('bg-black', 'text-white');
 }
+
+mainContainer.addEventListener('click', function(event){
+    console.log(event.target.parentNode.parentNode);
+});
